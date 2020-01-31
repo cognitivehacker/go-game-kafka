e2a7dc8962f470031f0ec77835f8a504b09d4275
Alan Yoshida <alanyoshida@msn.com>
2020-01-31 17:39:51 -0300

    Init

---
+++ cmd/client.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+func main() {
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	log.Println("Creating window")
+
+	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		800, 600, sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	surface, err := window.GetSurface()
+	if err != nil {
+		panic(err)
+	}
+	surface.FillRect(nil, 0)
+
+	rect := sdl.Rect{0, 0, 200, 200}
+	surface.FillRect(&rect, 0xffee0000)
+	window.UpdateSurface()
+
+	log.Println("Start running")
+	running := true
+	for running {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				println("Quit")
+				running = false
+				break
+			}
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-game-kafka
+
+go 1.12
+
+require github.com/veandco/go-sdl2 v0.4.0

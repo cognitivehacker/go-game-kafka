44d52a6a072f18782e1fade580c4879b1794579f
Alan Yoshida <alanyoshida@msn.com>
2020-02-04 12:21:33 -0300

    Refactoring: now have a Player struct with draw function

--- cmd/client.go
+++ cmd/client.go
@@ -6,11 +6,31 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
+type Player struct {
+	x      int32
+	y      int32
+	width  int32
+	height int32
+}
+
+func (p Player) draw(window **sdl.Window) {
+	w := *window
+	pl1 := sdl.Rect{p.x, p.y, p.width, p.height}
+	surface, err := w.GetSurface()
+	if err != nil {
+		panic(err)
+	}
+	surface.FillRect(&pl1, 0xffee0000)
+	w.UpdateSurface()
+}
+
 func main() {
+	var window *sdl.Window
 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
 		panic(err)
 	}
 	defer sdl.Quit()
+
 	log.Println("Creating window")
 	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
 		800, 600, sdl.WINDOW_SHOWN)
@@ -24,29 +44,36 @@ func main() {
 	}
 	log.Println("Start running")
 	running := true
-	var x int32 = 200
-	var y int32 = 200
+	newPlayer := Player{x: 300, y: 300, width: 10, height: 10}
+
 	for running {
+		// Clear window for each frame
 		surface.FillRect(nil, 0)
 
-		pl1 := sdl.Rect{x, y, 20, 20}
-		pl2 := sdl.Rect{400, 400, 20, 20}
-		surface.FillRect(&pl1, 0xffee0000)
-		surface.FillRect(&pl2, 0xffeeff00)
+		newPlayer.draw(&window)
+
 		window.UpdateSurface()
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch t := event.(type) {
 			case *sdl.KeyboardEvent:
-				// keyCode := t.Keysym.Sym
-				keys := ""
 				// Modifier keys
 				switch t.Keysym.Sym {
 				case sdl.K_UP:
-					if t.State == sdl.RELEASED {
-						y--
-						println("Foo Balr")
+					if t.State == sdl.PRESSED {
+						newPlayer.y -= 5
 					}
-					keys += "Left Alt"
+				case sdl.K_DOWN:
+					if t.State == sdl.PRESSED {
+						newPlayer.y += 5
+					}
+				case sdl.K_LEFT:
+					if t.State == sdl.PRESSED {
+						newPlayer.x -= 5
+					}
+				case sdl.K_RIGHT:
+					if t.State == sdl.PRESSED {
+						newPlayer.x += 5
+					}
 				}
 			case *sdl.QuitEvent:
 				println("Quit")

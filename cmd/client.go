a67e63970ed548af05a0176c2c26e9a23c554438
Alan Yoshida <alanyoshida@msn.com>
2020-02-05 19:15:11 -0300

    Needs waitgroup for goroutines

--- cmd/client.go
+++ cmd/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"math/rand"
@@ -14,44 +15,79 @@ import (
 )
 
 type Player struct {
-	uuid   uuid.UUID
-	x      int32
-	y      int32
-	width  int32
-	height int32
-	color  uint32
+	Uuid   uuid.UUID
+	X      int32
+	Y      int32
+	Width  int32
+	Height int32
+	Color  uint32
 }
-
-func (p Player) draw(window **sdl.Window) {
-	w := *window
-	pl1 := sdl.Rect{p.x, p.y, p.width, p.height}
-	surface, err := w.GetSurface()
-	if err != nil {
-		panic(err)
-	}
-	if p.color == 0 {
-		p.color = 0xffee0000
-	}
-	surface.FillRect(&pl1, p.color)
-	// w.UpdateSurface()
-}
-
-func randomPlayer() Player {
-	return Player{
-		x:      int32(rand.Intn(800)),
-		y:      int32(rand.Intn(600)),
-		width:  10,
-		height: 10,
-		color:  0xffee0000}
+type KfkPlayerData struct {
+	Uuid   uuid.UUID `json:"uuid"`
+	X      int32     `json:"x"`
+	Y      int32     `json:"y"`
+	Width  int32     `json:"Width"`
+	Height int32     `json:"Height"`
+	Color  uint32    `json:"Color"`
 }
 
 func main() {
+	log.Println("Start running")
+	messages := make(chan KfkPlayerData)
+
+	// INITIATE KAFKA CONSUMER
+	go consumer(messages)
+
+	// MAP OF PLAYERS
+	players := make(map[uuid.UUID]*Player)
+
+	// CREATE MY PLAYER
+	var playerUUID uuid.UUID = uuid.New()
+	players[playerUUID] = &Player{
+		Uuid:   playerUUID,
+		X:      int32(rand.Intn(800)),
+		Y:      int32(rand.Intn(600)),
+		Width:  10,
+		Height: 10,
+		Color:  0xffee0000}
+
+	go gameLoop(players, playerUUID)
+
+	// // CONSUME KAFKA MESSAGES
+	updatedData := <-messages
+
+	// Player already in map
+	if val, ok := players[updatedData.Uuid]; ok {
+		fmt.Printf("Player ALREADY Exists %d", updatedData.Uuid)
+		println("", val)
+		// player := players[updatedData.Uuid]
+		// player.Uuid = updatedData.Uuid
+		// player.X = updatedData.X
+		// player.Y = updatedData.Y
+		// player.Width = updatedData.Width
+		// player.Height = updatedData.Height
+		// player.Color = updatedData.Color
+		// Player is new
+	} else {
+		fmt.Printf("Player IS NEW %d", updatedData.Uuid)
+		// players[updatedData.Uuid] = &Player{
+		// 	Uuid:   updatedData.Uuid,
+		// 	X:      updatedData.X,
+		// 	Y:      updatedData.Y,
+		// 	Width:  updatedData.Width,
+		// 	Height: updatedData.Height,
+		// 	Color:  updatedData.Color}
+	}
+
+}
+
+func gameLoop(players map[uuid.UUID]*Player, playerUUID uuid.UUID) {
+	// Create Game window
 	var window *sdl.Window
 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
 		panic(err)
 	}
 	defer sdl.Quit()
-
 	log.Println("Creating window")
 	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
 		800, 600, sdl.WINDOW_SHOWN)
@@ -63,32 +99,21 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	log.Println("Start running")
-	running := true
-
-	// MAP OF PLAYERS
-	players := make(map[uuid.UUID]*Player)
-	var playerUUID uuid.UUID = uuid.New()
-	players[playerUUID] = &Player{
-		uuid:   playerUUID,
-		x:      int32(rand.Intn(800)),
-		y:      int32(rand.Intn(600)),
-		width:  10,
-		height: 10,
-		color:  0xffee0000}
-
 	// GAME LOOP
+	running := true
 	for running {
 		// Clear window for each frame
 		surface.FillRect(nil, 0)
 
+		// DRAW PLAYERS
 		for _, p := range players {
 			p.draw(&window)
 		}
 		window.UpdateSurface()
 
-		player := players[playerUUID]
+		myPlayer := players[playerUUID]
 
+		// KEYBOARD EVENTS
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch t := event.(type) {
 			case *sdl.KeyboardEvent:
@@ -96,21 +121,24 @@ func main() {
 				switch t.Keysym.Sym {
 				case sdl.K_UP:
 					if t.State == sdl.PRESSED {
-						player.y -= 5
+						myPlayer.Y -= 5
 					}
 				case sdl.K_DOWN:
 					if t.State == sdl.PRESSED {
-						player.y += 5
+						myPlayer.Y += 5
 					}
 				case sdl.K_LEFT:
 					if t.State == sdl.PRESSED {
-						player.x -= 5
+						myPlayer.X -= 5
 					}
 				case sdl.K_RIGHT:
 					if t.State == sdl.PRESSED {
-						player.x += 5
+						myPlayer.X += 5
 					}
 				}
+
+				// PRODUCE KAFKA MESSAGE
+				go produce(*myPlayer)
 			case *sdl.QuitEvent:
 				println("Quit")
 				running = false
@@ -120,7 +148,29 @@ func main() {
 	}
 }
 
-func consumer() {
+func (p Player) draw(window **sdl.Window) {
+	w := *window
+	pl1 := sdl.Rect{p.X, p.Y, p.Width, p.Height}
+	surface, err := w.GetSurface()
+	if err != nil {
+		panic(err)
+	}
+	if p.Color == 0 {
+		p.Color = 0xffee0000
+	}
+	surface.FillRect(&pl1, p.Color)
+}
+
+func randomPlayer() Player {
+	return Player{
+		X:      int32(rand.Intn(800)),
+		Y:      int32(rand.Intn(600)),
+		Width:  10,
+		Height: 10,
+		Color:  0xffee0000}
+}
+
+func consumer(messages chan<- KfkPlayerData) {
 	// get kafka reader using environment variables.
 	kafkaURL := "localhost:29092"
 	topic := "mytopic"
@@ -136,27 +186,34 @@ func consumer() {
 		if err != nil {
 			log.Fatalln(err)
 		}
-		fmt.Printf("message at topic:%v partition:%v offset:%v	%s = %s\n", m.Topic, m.Partition, m.Offset, string(m.Key), string(m.Value))
+		var response KfkPlayerData
+		json.Unmarshal([]byte(m.Value), &response)
+		messages <- response
+
+		fmt.Printf("message at topic:%v partition:%v offset:%v	%s = %s\n",
+			m.Topic, m.Partition, m.Offset, string(m.Key), string(m.Value))
 	}
 }
 
-func producer() {
+func produce(player Player) {
+	playerJson, err := json.Marshal(player)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	kafkaURL := "localhost:29092"
 	topic := "mytopic"
 	writer := newKafkaWriter(kafkaURL, topic)
 	defer writer.Close()
-	fmt.Println("start producing ... !!")
-	for i := 0; ; i++ {
-		msg := kafka.Message{
-			Key:   []byte(fmt.Sprintf("Key-%d", i)),
-			Value: []byte(fmt.Sprint(uuid.New())),
-		}
-		err := writer.WriteMessages(context.Background(), msg)
-		if err != nil {
-			fmt.Println(err)
-		}
-		time.Sleep(1 * time.Second)
+	msg := kafka.Message{
+		Key:   []byte(fmt.Sprintf("Key-%d", player.Uuid)),
+		Value: []byte(fmt.Sprint(string(playerJson))),
 	}
+	err2 := writer.WriteMessages(context.Background(), msg)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	time.Sleep(1 * time.Second)
 }
 
 func newKafkaWriter(kafkaURL, topic string) *kafka.Writer {
